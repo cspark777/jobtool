7a0b2f764dacb26468f85738c993d27aabed1d42
Victor L <rhettb198501@hotmail.com>
2021-03-08 14:41:20 -0300

    fix

--- ui/app.py
+++ ui/app.py
@@ -197,4 +197,4 @@ def export():
                  "attachment; filename=results.csv"})
 
 
-app.run(debug = False, host="0.0.0.0", port="9080", use_reloader=False,)
+app.run(debug = False, host="0.0.0.0", port="8080", use_reloader=False,)
